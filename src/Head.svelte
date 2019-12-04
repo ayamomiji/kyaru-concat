0c95519ee0951eb2b114d93729e407c9c2e512be
ayaya zhao <ayaya.zhao@gmail.com>
2019-12-04 17:28:41 +0800

    lesser control nodes

--- src/Head.svelte
+++ src/Head.svelte
@@ -11,8 +11,17 @@
     const image = new Image()
     image.src = kyaru
     image.onload = function () {
-      fabricImage = new fabric.Image(image)
+      fabricImage = new fabric.Image(image, {
+        rotatingPointOffset: 15,
+        cornerStyle: 'circle'
+      })
+      fabricImage.setControlVisible('ml', false)
+      fabricImage.setControlVisible('mt', false)
+      fabricImage.setControlVisible('mr', false)
+      fabricImage.setControlVisible('mb', false)
       canvas.add(fabricImage)
+      canvas.centerObject(fabricImage)
+      canvas.setActiveObject(fabricImage)
     }
   })
 
