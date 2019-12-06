0600e3dd3dbc20e462b12a69c72e947b17334e9d
ayaya zhao <ayaya.zhao@gmail.com>
2019-12-07 00:42:06 +0800

    fix wrong path

--- src/images.js
+++ src/images.js
@@ -1,6 +1,6 @@
 import kyaru from './images/kyaru.png'
 import kyaru2 from './images/kyaru2.png'
 import kyaru3 from './images/kyaru3.png'
-import kyaru4 from './images/kyaru3.png'
+import kyaru4 from './images/kyaru4.png'
 
 export default [kyaru, kyaru2, kyaru3, kyaru4]
