1e63a3e13699fe2cbd3b3a19051502046c601834
ayaya zhao <ayaya.zhao@gmail.com>
2019-12-07 22:12:43 +0800

    make footer more obviously >_<

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,12 +1,22 @@
-<div class='text-muted small text-center'>
-  網站開發: <a href='http://ayaya.tw' target='_blank'>ayaya.tw</a>
-  特別感謝: <a href='https://www.pixiv.net/member.php?id=5676585' target='_blank'>
-    星爆大師 YuyuSuika
-  </a>
-</div>
+<style>
+  .footer {
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+</style>
 
-<div class='text-muted small text-center'>
-  本站為粉絲自製, 與 Cygames, So-net Taiwan 皆無任何關係,
-  本網站使用的遊戲圖像的版權,
-  商標權和其他知識產權屬於原內容提供者 (So-net Taiwan, Cygames).
+<div class='footer position-fixed bg-white'>
+  <div class='text-muted small text-center'>
+    網站開發: <a href='http://ayaya.tw' target='_blank'>ayaya.tw</a>
+    特別感謝: <a href='https://www.pixiv.net/member.php?id=5676585' target='_blank'>
+      星爆大師 YuyuSuika
+    </a>
+  </div>
+
+  <div class='text-muted small text-center'>
+    本站為粉絲自製, 與 Cygames, So-net Taiwan 皆無任何關係,
+    本網站使用的遊戲圖像的版權,
+    商標權和其他知識產權屬於原內容提供者 (So-net Taiwan, Cygames).
+  </div>
 </div>
